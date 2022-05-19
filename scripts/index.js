ca0c5ccc057195f2e4dbcd4aeee14d9b58d08160
shaheenhad <shaheenhad@gmail.com>
2022-05-19 12:01:56 -0400

    feat: add new card popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,10 @@
 const page = document.querySelector(".page");
 const editButton = page.querySelector(".profile__edit-button");
-const popup = page.querySelector(".popup");
-const closeButton = page.querySelector(".popup__close");
+const addButton = page.querySelector(".profile__add-button");
+const editPopup = page.querySelector(".popup_edit-button");
+const addPopup = page.querySelector(".popup_add-button");
+const closeButtonEdit = page.querySelector(".popup__close_edit");
+const closeButtonAdd = page.querySelector(".popup__close_add");
 const profileName = page.querySelector(".profile__name");
 const profileTitle = page.querySelector(".profile__description");
 const popupName = page.querySelector(".popup__input_type_name");
@@ -11,7 +14,7 @@ const popupForm = page.querySelector(".popup__form");
 // make popup visible when edit button is clicked
 
 function editProfile() {
-  popup.classList.add("popup_is-visible");
+  editPopup.classList.add("popup_is-visible");
 
   // keep profile info when popup is opened
 
@@ -21,8 +24,8 @@ function editProfile() {
 
 // close popup when close button is clicked
 
-function closePopup() {
-  popup.classList.remove("popup_is-visible");
+function closeEditPopup() {
+  editPopup.classList.remove("popup_is-visible");
 }
 
 // save new input data when save button is used
@@ -33,9 +36,20 @@ function saveProfile(e) {
   profileName.textContent = popupName.value;
   profileTitle.textContent = popupTitle.value;
 
-  closePopup();
+  closeEditPopup();
+}
+
+// add button and popup functionality
+function addButtonHandler() {
+  addPopup.classList.add("popup_is-visible");
+}
+
+function closeAddPopup() {
+  addPopup.classList.remove("popup_is-visible");
 }
 
 editButton.addEventListener("click", editProfile);
-closeButton.addEventListener("click", closePopup);
+closeButtonEdit.addEventListener("click", closeEditPopup);
 popupForm.addEventListener("submit", saveProfile);
+addButton.addEventListener("click", addButtonHandler);
+closeButtonAdd.addEventListener("click", closeAddPopup);
