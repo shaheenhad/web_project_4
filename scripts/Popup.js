0cc771818d446f07360e624d414900d99d78c7b9
shaheenhad <shaheenhad@gmail.com>
2022-07-12 19:09:54 -0400

    begin refactor of popup functionality

--- scripts/Popup.js
+++ scripts/Popup.js
@@ -1,5 +1,21 @@
 class Popup {
   constructor(popupSelector) {
-    this._popupSelector = popupSelector;
+    this._popup = document.querySelector(popupSelector);
   }
+
+  open() {
+    this._popup.classList.add("popup_is-visible")
+  }
+
+  close() {
+    this._popup.classList.remove("popup_is-visible")
+  }
+
+  _handleEscClose(evt) {
+    if (evt.key === "Escape") {
+      this.close(this._popup);
+    }
+  }
+
+  setEventListeners()
 }
---
+++ scripts/UserInfo.js
@@ -0,0 +1,16 @@
+class UserInfo {
+  constructor({ usernameSelector, jobSelector }) {
+    this._username = document.querySelector(usernameSelector);
+    this._job = document.querySelector(jobSelector);
+  }
+
+  getUserInfo() {
+    const userObj = {
+      username: this._username.textContent,
+      job: this._job.textContent,
+    };
+    return userObj;
+  }
+
+  setUserInfo() {}
+}
